f3d5456de895562c56f8a7a69c189a563a454f0d
Pratik Naik @ India <pratikway.90@gmail.com>
2020-05-05 12:49:00 +0530

    Update redis-install-script.sh

--- redis-install-script.sh
+++ redis-install-script.sh
@@ -16,9 +16,9 @@ yum -q -y install gcc gcc-c++ make
 ####
 # Download and install Redis:
 ####
-wget -q http://download.redis.io/releases/redis-6.0.0.tar.gz
-tar xzf redis-6.0.0.tar.gz
-rm -f redis-6.0.0.tar.gz
+wget -q http://download.redis.io/releases/redis-5.0.9.tar.gz
+tar xzf redis-5.0.9.tar.gz
+rm -f redis-5.0.9.tar.gz
 cd redis-stable
 make -s
 make -s install
